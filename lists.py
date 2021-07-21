c1f45bd2facc040da990ddc891b715805943dae8
David Luna <davidluna@mail.weber.edu>
2021-07-21 01:36:53 -0600

    Started working on lists

---
+++ lists.py
@@ -0,0 +1,10 @@
+mine = ['a promised', 'land', 'hehe']
+print(mine)
+
+yes = ['cabbage', 'eggplant', 'watermelon']
+if 'watermelon' in yes:
+    print('yes, watermelon is present in the list \'yes\'')
+
+wait_list = ['amanda', 'fiber', 'oliver']
+if 'oliver' in wait_list:
+    print('yes he is on the wait list')
