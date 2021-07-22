9cbe6b10a0c64a2b1c0991cd5b8cce3606fedcde
David Luna <davidluna@mail.weber.edu>
2021-07-22 04:51:01 -0600

    started with accessing list items

--- lists.py
+++ lists.py
@@ -8,3 +8,13 @@ if 'watermelon' in yes:
 wait_list = ['amanda', 'fiber', 'oliver']
 if 'oliver' in wait_list:
     print('yes he is on the wait list')
+
+# Accessing list items
+one = ['first series', 'second series', 'third']
+print(one[2])
+one_ = one[1:2]
+print(one_)
+
+two = ['fifty', 'shades', 'of grey', 'is', 'cool', 'yes']
+print(two[:4])  # By leaving out the start value, the range will start at the first item
+print(two[3:])  # By leaving out the end value, the range will go on to the end of the list
