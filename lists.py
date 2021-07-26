d8afa655c37e6c73e2daef82eefdd97f1076ba95
David Luna <davidluna@mail.weber.edu>
2021-07-26 15:28:33 -0600

    started with changing list items

--- lists.py
+++ lists.py
@@ -18,3 +18,23 @@ print(one_)
 two = ['fifty', 'shades', 'of grey', 'is', 'cool', 'yes']
 print(two[:4])  # By leaving out the start value, the range will start at the first item
 print(two[3:])  # By leaving out the end value, the range will go on to the end of the list
+
+water = ['cool water', 'lukewarm', 'cold', 'hot']
+print(water[:3])
+print(water[2:])
+
+comp = ['red one', 'blue one', 'grey oone', 'pink', 'food', 'yes']
+print(comp[-5:-2])
+
+list_of_food = ['chips', 'soda', 'candy', 'potatoes', 'eggplant', 'banana']
+print(list_of_food[3:-1])
+print(list_of_food[-6:-3])
+
+list_of_students = ['mia', 'lial', 'ria', 'code', 'yep', 'yurpa']
+if 'yurpa' in list_of_students:
+    print('yes yurpa is there')
+
+# Changing list items
+this_list = ['iphone', 'android', 'nokia', 'sprint']
+this_list[3] = 'galactic'
+print(this_list)
